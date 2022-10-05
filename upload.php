a84221250e9b9f9e6bea94a03608694f1c14595e
SihleLucingo <110468067+SihleLucingo@users.noreply.github.com>
2022-10-05 11:31:23 +0200

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Soso CPT</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+<body>
+        <form method="post" enctype="multipart/form-data">
+            <label>file upload</label>
+            <input type="file" name="file">|
+            <input type="submit" name="submit">
+
+        </form>
+</body>
+</html>
